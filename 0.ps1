8e61408d1f3915fbbd1d8d19bd7ae721a7351e18
mokka-official <sakuraiyukino_official@outlook.com>
2026-01-08 16:43:56 +0800

    推送脚本

---
+++ 0.ps1
@@ -0,0 +1,6 @@
+$host.ui.RawUI.WindowTitle = 'Terminal'
+ping.exe -n 1 -w 1000 baidu.com >$null 2>$null
+$reach = $LASTEXITCODE -eq 0
+Write-Host $(if ($reach){'BAIDU REACHABLE'}else{'BAIDU UNREACHABLE'}) `
+            -ForegroundColor $(if ($reach){'Green'}else{'Red'})
+Start-Sleep -Seconds 2
